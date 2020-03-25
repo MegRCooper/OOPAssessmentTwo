bac038eb684aee19abed12bcbf12b7ad7efd3952
MegRCooper <58695342+MegRCooper@users.noreply.github.com>
2020-03-25 14:22:08 +0000

    Added files via upload
    Code for the OOP Assessment One - first upload.

---
+++ OOPAssessmentTwoV2/OOPAssessmentTwoV2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace OOPAssessmentTwoV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var fileOne = Path.Combine(Directory.GetCurrentDirectory(), "\\GitRepositories_1a.txt");
+            string fileOne = File.ReadAllText("C:\\Users\\megrc\\source\\repos\\OOPAssessmentTwoV2\\OOPAssessmentTwoV2\\Resources\\GitRepositories_1a.txt");
+            string fileTwo = File.ReadAllText("C:\\Users\\megrc\\source\\repos\\OOPAssessmentTwoV2\\OOPAssessmentTwoV2\\Resources\\GitRepositories_1b.txt");
+
+            int linematch = 0;
+            int fileOneLen = fileOne.Length;
+            int fileTwoLen = fileTwo.Length;
+            
+            if (fileOneLen == fileTwoLen) //Checks the byte len.
+            {
+                for (int i = 0; i < fileOneLen; i++) //Checks the lines one at a time.
+                {
+                    if (fileOne[i] == fileTwo[i])
+                    {
+                        linematch++; //Checks if all lines match
+                    }
+                }
+                if (linematch == fileOneLen) //If all lines match, gives good output
+                {
+                    Console.WriteLine("[OUTPUT] Files match");
+                }
+                else
+                {
+                    Console.WriteLine("[OUTPUT] Files do not match");
+                }
+            }
+            else //If lengths do not match
+            {
+                Console.WriteLine("[OUTPUT] Files do not match");
+            }
+        }
+
+    }
+}
